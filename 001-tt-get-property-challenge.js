a51ecc2282e0f8f580302d133c6fa3088f63c6f5
iyadshobaki <iyadshobaki82@gmail.com>
2026-01-05 16:55:14 -0500

    whiteboard: add get property challenge

---
+++ 001-tt-get-property-challenge.js
@@ -0,0 +1,45 @@
+//Tripleten whiteboard practice
+
+function getProperty(obj, path) {
+  // Tripleten suggested solution
+  // split the path into an array and save the result in the arr variable
+  const arr = path.split(".");
+  // define the res variable and assign obj to it
+  let res = obj;
+  // iterate over the array from the first element to the last
+  for (let i = 0; i < arr.length; i += 1) {
+    // pick an array element
+    // get the property from the res variable by key
+    // save the retrieved property in the res variable
+    res = res[arr[i]];
+  }
+
+  return res;
+}
+
+const object = {
+  one: 1,
+  two: {
+    three: 3,
+  },
+  four: 4,
+};
+
+///
+console.log(getProperty(object, "one")); // 1
+console.log(getProperty(object, "two.three")); // 3
+
+///////////////
+function getPropertyIyad(obj, path) {
+  // write your code here
+  // My solution - Iyad
+  const pathSplit = path.split(".");
+  let variable = "obj";
+  pathSplit.forEach((p) => {
+    variable = variable + `["${p}"]`;
+  });
+
+  console.log(eval(variable));
+}
+getPropertyIyad(object, "one"); // 1
+getPropertyIyad(object, "two.three"); // 3
